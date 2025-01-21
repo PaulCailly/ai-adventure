92f62ea52736f64a7e412d4bc8544cf34d25567e
pcailly <pcailly@pm.me>
2025-01-21 22:48:47 +0100

    fix: remove tooltip

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -5,28 +5,22 @@ import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "./icons";
 import { memo } from "react";
-import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 function PureChatHeader() {
   const router = useRouter();
 
   return (
     <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <Button
-            variant="outline"
-            className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-auto md:ml-0"
-            onClick={() => {
-              router.push("/");
-              router.refresh();
-            }}
-          >
-            <PlusIcon />
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>New</TooltipContent>
-      </Tooltip>
+      <Button
+        variant="outline"
+        className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-auto md:ml-0"
+        onClick={() => {
+          router.push("/");
+          router.refresh();
+        }}
+      >
+        <PlusIcon />
+      </Button>
     </header>
   );
 }
