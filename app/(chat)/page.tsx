b420f0da432a9b0775622da88dbee8e6ad3d3dc2
pcailly <pcailly@pm.me>
2025-04-13 11:05:09 +0200

    feat: add redirection to character page upon re login

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,9 +1,24 @@
+import { redirect } from "next/navigation";
 import { Chat } from "@/components/chat";
 
 import { generateUUID } from "@/lib/utils";
 import { DataStreamHandler } from "@/components/data-stream-handler";
+import { getMostRecentChat } from "@/lib/db/queries";
+import { auth } from "@/app/(auth)/auth";
 
 export default async function Page() {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    return redirect("/login");
+  }
+
+  const mostRecentChat = await getMostRecentChat({ userId: session.user.id });
+
+  if (mostRecentChat) {
+    return redirect(`/chat/${mostRecentChat.id}`);
+  }
+
   const id = generateUUID();
 
   return (
