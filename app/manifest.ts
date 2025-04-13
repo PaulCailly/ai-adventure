6f2c1eda0dc52570dcbefd2ed7278284c98b9065
pcailly <pcailly@pm.me>
2025-04-13 02:02:39 +0200

    feat: add pwa support

---
+++ app/manifest.ts
@@ -0,0 +1,13 @@
+import { type MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "La Taverne de l'Union",
+    short_name: "La Taverne de l'Union",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#000000",
+    theme_color: "#000000",
+  };
+}
